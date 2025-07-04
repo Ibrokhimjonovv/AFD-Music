2ea834beee75d826121929421c94d5fa36e7c31b
Ibrokhimjonovv <megabytekekabyte@gmail.com>
2025-07-04 16:07:30 +0500

    .

--- src/components/mobile/mobile-mini-player/mobile-mini-player.jsx
+++ src/components/mobile/mobile-mini-player/mobile-mini-player.jsx
@@ -5,7 +5,7 @@ import React, { useContext, useEffect, useRef, useState } from 'react'
 import "./mobile-mini-player.scss";
 
 const MobileMiniPlayer = () => {
-    const { playingSong, isPlaying, togglePlayPause, audioRef, setIsPlaying, playNextSong, isRepeat, isLiked } = useContext(AccessContext);
+    const { playingSong, isPlaying, togglePlayPause, audioRef, setIsPlaying, playNextSong, isRepeat, toggleLike, likedSongs } = useContext(AccessContext);
     const [progress, setProgress] = useState(0);
     const [duration, setDuration] = useState(0);
     const [currentTime, setCurrentTime] = useState(0);
@@ -44,6 +44,10 @@ const MobileMiniPlayer = () => {
         setProgress(seekPercentage);
         setCurrentTime(seekTime);
     };
+
+    const isLiked = likedSongs.some((s) => s.id === playingSong?.id);
+
+
     return (
         <div className='mini-player'>
             <div className="full-line">
@@ -81,9 +85,14 @@ const MobileMiniPlayer = () => {
                                     role="img"
                                     aria-hidden="true"
                                     className="e-9960-icon e-9960-baseline"
-                                    viewBox="0 0 24 24"
+                                    viewBox="0 0 16 16"
+                                    style={{
+                                        '--encore-icon-height': 'var(--encore-graphic-size-decorative-smaller)',
+                                        '--encore-icon-width': 'var(--encore-graphic-size-decorative-smaller)',
+                                    }}
                                 >
-                                    <path d="M5.21 1.57a6.76 6.76 0 0 1 6.708 1.545.124.124 0 0 0 .165 0 6.74 6.74 0 0 1 5.715-1.78l.004.001a6.8 6.8 0 0 1 5.571 5.376v.003a6.69 6.69 0 0 1-1.49 5.655l-7.954 9.48a2.518 2.518 0 0 1-3.857 0L2.12 12.37A6.68 6.68 0 0 1 .627 6.714 6.76 6.76 0 0 1 5.21 1.57m3.12 1.803a4.757 4.757 0 0 0-5.74 3.725l-.001.002a4.68 4.68 0 0 0 1.049 3.969l.009.01 7.958 9.485a.518.518 0 0 0 .79 0l7.968-9.495a4.69 4.69 0 0 0 1.049-3.965 4.8 4.8 0 0 0-3.931-3.794 4.74 4.74 0 0 0-4.023 1.256l-.008.008a2.123 2.123 0 0 1-2.9 0l-.007-.007a4.76 4.76 0 0 0-2.214-1.194z" />
+                                    <path d="M8 1.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8" />
+                                    <path d="M11.75 8a.75.75 0 0 1-.75.75H8.75V11a.75.75 0 0 1-1.5 0V8.75H5a.75.75 0 0 1 0-1.5h2.25V5a.75.75 0 0 1 1.5 0v2.25H11a.75.75 0 0 1 .75.75" />
                                 </svg>
 
                             )
